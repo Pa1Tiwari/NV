f36d45321c686e1db3652b6145cbc7a4c887a323
Pawan Tiwari <pawantiwari4406@gmail.com>
2026-08-24 20:08:21 +0530

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 // Replace these values before deploying.
 // You can also rename config.example.js to config.js and edit it.
 window.NV_CONFIG = {
-  SUPABASE_URL: "https://neilyvynasdhvtztzfnb.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5laWx5dnluYXNkaHZ0enR6Zm5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1Nzk2OTAsImV4cCI6MjEwMzE1NTY5MH0.qQQAewRyH4ZuflGc9YAn7q2u4o-XVv93p6QFjzjEg_A",
+  SUPABASE_URL:https://neilyvynasdhvtztzfnb.supabase.co,
+  SUPABASE_ANON_KEY:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5laWx5dnluYXNkaHZ0enR6Zm5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1Nzk2OTAsImV4cCI6MjEwMzE1NTY5MH0.qQQAewRyH4ZuflGc9YAn7q2u4o-XVv93p6QFjzjEg_A,
   ADMIN_EMAIL: "your-admin-email@example.com"
 };
