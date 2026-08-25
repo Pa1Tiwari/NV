1ea65b20e175fcba2ad1e24e08a74618c966f298
Pawan Tiwari <pawantiwari4406@gmail.com>
2026-08-25 13:39:54 +0530

    Update ADMIN_EMAIL in config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.NV_CONFIG = {
   SUPABASE_URL: "https://neilyvynasdhvtztzfnb.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5laWx5dnluYXNkaHZ0enR6Zm5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1Nzk2OTAsImV4cCI6MjEwMzE1NTY5MH0.qQQAewRyH4ZuflGc9YAn7q2u4o-XVv93p6QFjzjEg_A",
-  ADMIN_EMAIL: "pawantiwariii4406@gmail.com"
+  ADMIN_EMAIL: "sagardubey513@gmail.com"
 };
